81195b4a624aad40222cbcbc82425979efea3353
towfiqu <towfiqu@gmail.com>
2020-07-07 20:02:35 +0600

    database added

--- index.js
+++ index.js
@@ -17,3 +17,31 @@ app.get('/', async (req, res) => {
     status: `Automation working from ${process.env.LOCATION}`,
   });
 });
+
+app.get('/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const phone = await getPhone(id);
+  res.json({status: 'Success', data: {phone: phone}});
+});
+
+let cachedDbPool;
+function getDbPool() {
+  if (!cachedDbPool) {
+    mysql.createPool({
+      connectionLimit: 1,
+      user: process.env.SQL_USER,
+      database: process.env.SQL_NAME,
+      socketPath: `/cloudsql/${process.env.INST_CON_NAME}`,
+    });
+  }
+  return cachedDbPool;
+}
+
+const getPhone = async id => {
+  return new Promise(function (resolve, reject) {
+    const sql = 'SELECT * FROM phones WHERE id = ?';
+    getDbPool().query(sql, [id], (err, results) => {
+      resolve(results[0]);
+    });
+  });
+};
